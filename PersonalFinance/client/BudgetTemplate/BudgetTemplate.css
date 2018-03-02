3418217c7fc7d30b6ebca5ba46ffce800e7260d7
XeXeede <32223170+XeXeede@users.noreply.github.com>
2018-03-02 17:45:39 -0600

    Create BudgetTemplate.css

---
+++ PersonalFinance/client/BudgetTemplate/BudgetTemplate.css
@@ -0,0 +1,24 @@
+.pageTitle {
+    padding-top: 50px;
+}
+
+.content {
+    padding-left: 6em;
+    color:  rgb(13, 79, 192);
+}
+
+li {
+    font-size: 20px;
+}
+
+.paragraphContent {
+    text-align: center;
+    padding-top: 2em;
+}
+
+.videoContent {
+    padding: 20px;
+    text-align: center;
+    margin: 0 auto;
+    display: block;
+}
